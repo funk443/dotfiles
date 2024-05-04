34f0b505dad0a54c25ce88475479c3ed8d6302f1
CToID <funk443@yandex.com>
2024-05-05 02:33:20 +0800

    New Emacs config.
    Now a nice and simple emacs config.  "<TAB>" and "M-i" are switched to
    mimic the "autoindent" feature found in Vim.

---
+++ .config/emacs/init.el
@@ -0,0 +1,63 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(set-face-attribute 'default nil :family "JetBrains Mono" :height 240)
+(set-face-attribute 'fixed-pitch nil :inherit 'default)
+
+(use-package ligature
+  :ensure t
+  :config
+  (ligature-set-ligatures t '("-|" "-~" "---" "-<<" "-<" "--" "->" "->>" "-->" "///" "/=" "/=="
+                              "/>" "//" "/*" "*>" "***" "*/" "<-" "<<-" "<=>" "<=" "<|" "<||"
+                              "<|||" "<|>" "<:" "<>" "<-<" "<<<" "<==" "<<=" "<=<" "<==>" "<-|"
+                              "<<" "<~>" "<=|" "<~~" "<~" "<$>" "<$" "<+>" "<+" "</>" "</" "<*"
+                              "<*>" "<->" "<!--" ":>" ":<" ":::" "::" ":?" ":?>" ":=" "::=" "=>>"
+                              "==>" "=/=" "=!=" "=>" "===" "=:=" "==" "!==" "!!" "!=" ">]" ">:"
+                              ">>-" ">>=" ">=>" ">>>" ">-" ">=" "&&&" "&&" "|||>" "||>" "|>" "|]"
+                              "|}" "|=>" "|->" "|=" "||-" "|-" "||=" "||" ".." ".?" ".=" ".-" "..<"
+                              "..." "+++" "+>" "++" "[||]" "[<" "[|" "{|" "??" "?." "?=" "?:" "##"
+                              "###" "####" "#[" "#{" "#=" "#!" "#:" "#_(" "#_" "#?" "#(" ";;" "_|_"
+                              "__" "~~" "~~>" "~>" "~-" "~@" "$>" "^=" "]#"))
+  (global-ligature-mode t))
+
+(use-package magit
+  :ensure t)
+
+(keymap-global-set "<TAB>" 'tab-to-tab-stop)
+(keymap-global-set "M-i" 'indent-for-tab-command)
+
+(defconst rubbish-dir (concat user-emacs-directory "rubbish")
+  "Directory for Emacs to dump auto save files and backup files.")
+(unless (file-directory-p rubbish-dir)
+  (make-directory rubbish-dir))
+
+(add-hook 'before-save-hook #'delete-trailing-whitespace)
+(setq auto-mode-alist nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-file-name-transforms `((".*" ,rubbish-dir t)))
+ '(backup-directory-alist `((".*" . ,rubbish-dir)))
+ '(column-number-mode t)
+ '(delete-selection-mode t)
+ '(indent-tabs-mode nil)
+ '(menu-bar-mode nil)
+ '(package-selected-packages '(magit ligature))
+ '(save-interprogram-paste-before-kill t)
+ '(scroll-bar-mode nil)
+ '(scroll-conservatively 101)
+ '(scroll-error-top-bottom t)
+ '(size-indication-mode t)
+ '(tab-width 2)
+ '(tool-bar-mode nil)
+ '(x-select-enable-clipboard-manager nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
