e8b428f29675f98d579c6e14c35b5674f00c6afe
CToID <funk443@yahoo.com.tw>
2024-10-31 13:24:29 +0800

    Rearrange configs with `use-package`.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,11 +1,19 @@
-;; Configuration options:
+;;; Configuration options:
 ;;   --prefix=/home/id/.local
 ;;   --with-native-compilation=yes
 ;;   --with-pgtk=yes
 ;;   --with-tree-sitter=yes
 ;;   CFLAGS="-O2 -g3"
 
-(load-theme 'modus-vivendi t)
+;;; Package repo configurations
+(use-package package
+  :custom
+  (package-archives
+   '(("melpa" . "https://melpa.org/packages/")
+     ("gnu" . "https://elpa.gnu.org/packages/")
+     ("nongnu" . "https://elpa.nongnu.org/nongnu/"))))
+
+;;; Fonts and themes configurations
 (set-face-attribute 'default nil
                     :family "IBM Plex Mono"
                     :height 280)
@@ -16,10 +24,29 @@
                     :inherit 'default
                     :family "IBM Plex Sans")
 
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+(use-package modus-themes
+  :init
+  (require-theme 'modus-themes)
+  :custom
+  (modus-themes-bold-constructs t)
+  (modus-themes-inhibit-reload nil)
+  (modus-themes-italic-constructs t)
+  (modus-themes-mixed-fonts t)
+  :config
+  (load-theme 'modus-vivendi t))
 
+;;; Built-in packages configurations.
+(use-package dired
+  :custom
+  (dired-dwim-target t)
+  (dired-listing-switches "-alh"))
+
+(use-package org
+  :custom
+  (org-src-preserve-indentation t)
+  (org-startup-truncated nil))
+
+;;; External packages configurations.
 (use-package magit
   :ensure t)
 
@@ -31,6 +58,7 @@
 (use-package vterm
   :ensure t)
 
+;;; Programming languages configurations
 (use-package markdown-mode
   :ensure t
   :hook
@@ -42,6 +70,17 @@
   :ensure t
   :after markdown-mode)
 
+(use-package c-ts-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.h\\'" . c-ts-mode))
+  :custom
+  (c-ts-mode-indent-offset 4)
+  (c-ts-mode-indent-style 'k&r)
+  :bind
+  (:map c-ts-mode-map
+        ("C-c C-\\" . my-c-format-buffer)))
+
+;;; My defined variables and functions.
 (defconst rubbish-dir (concat user-emacs-directory "rubbish")
   "Directory for Emacs to dump auto save files and backup files.")
 (unless (file-directory-p rubbish-dir)
@@ -50,15 +89,8 @@
 (defun my-remove-fireworks ()
   (when (< treesit-font-lock-level 2)
     (setq-local treesit-font-lock-feature-list
-                '((string comment)))
+                '((string comment preprocessor)))
     (treesit-font-lock-recompute-features)))
-
-(add-hook 'before-save-hook #'delete-trailing-whitespace)
-(add-hook 'prog-mode-hook #'my-remove-fireworks)
-
-(keymap-global-set "C-c C-p" #'compile)
-(keymap-global-unset "C-z")
-(keymap-global-unset "C-x C-z")
 
 (defun my-c-format-buffer ()
   (interactive)
@@ -72,13 +104,15 @@
         (goto-line line))
     (message "ERROR: `astyle` not found.")))
 
-(use-package c-ts-mode
-  :bind
-  (:map c-ts-mode-map
-        ("C-c C-\\" . my-c-format-buffer))
-  :init
-  (add-to-list 'auto-mode-alist '("\\.h\\'" . c-ts-mode)))
+;;; Hooks and keymaps.
+(add-hook 'before-save-hook #'delete-trailing-whitespace)
+(add-hook 'prog-mode-hook #'my-remove-fireworks)
 
+(keymap-global-set "C-c C-p" #'compile)
+(keymap-global-unset "C-z")
+(keymap-global-unset "C-x C-z")
+
+;;; Emacs custom stuffs.
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -87,35 +121,24 @@
  '(auth-source-save-behavior nil)
  '(auto-save-file-name-transforms `((".*" ,rubbish-dir t)))
  '(backup-directory-alist `((".*" \, rubbish-dir)))
- '(c-ts-mode-indent-offset 4)
- '(c-ts-mode-indent-style 'k&r)
  '(case-fold-search nil)
  '(case-replace nil)
  '(column-number-mode t)
  '(current-language-environment "UTF-8")
  '(dabbrev-case-fold-search nil)
  '(delete-selection-mode t)
- '(dired-dwim-target t)
- '(dired-listing-switches "-alh")
  '(fill-column 72)
  '(find-ls-option '("-exec ls -ldh {} +" . "-ldh"))
  '(font-lock-maximum-decoration nil)
  '(frame-resize-pixelwise t)
  '(indent-tabs-mode nil)
  '(indicate-empty-lines t)
- '(inferior-lisp-program "sbcl")
  '(inhibit-startup-screen t)
  '(initial-buffer-choice t)
  '(initial-frame-alist '((vertical-scroll-bars) (fullscreen . maximized)))
  '(initial-scratch-message ";; EMACS: Escape, Meta, Alt, Control, and Shift.\12\12")
  '(menu-bar-mode nil)
  '(mode-line-compact t)
- '(modus-themes-bold-constructs t)
- '(modus-themes-inhibit-reload nil)
- '(modus-themes-italic-constructs t)
- '(modus-themes-mixed-fonts t)
- '(org-src-preserve-indentation t)
- '(org-startup-truncated nil)
  '(package-selected-packages '(edit-indirect magit markdown-mode treesit-auto vterm))
  '(read-buffer-completion-ignore-case t)
  '(ring-bell-function 'ignore)
