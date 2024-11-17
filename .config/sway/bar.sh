3f594737d05f19e6145a2fed3233bcbd24f4008a
CToID <funk443@yahoo.com.tw>
2024-11-17 17:30:11 +0800

    Display battery percentage in bar.

--- .config/sway/bar.sh
+++ .config/sway/bar.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 while true; do
+    battery_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
+    battery_status=$(cat /sys/class/power_supply/BAT0/status)
     volume=$(pamixer --get-volume-human)
     date=$(date +'%Y-%m-%d %H:%M:%S')
-    echo "${volume} | ${date}"
+    echo "$battery_percentage% $battery_status | ${volume} | ${date}"
     sleep 1
 done
