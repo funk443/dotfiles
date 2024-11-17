38701c353eeede1812afdb3cf234a10a2effadad
CToID <funk443@yahoo.com.tw>
2024-11-17 10:59:13 +0800

    Add Neovide configs.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -59,3 +59,21 @@ vim.opt.undofile = true
 vim.g.netrw_browsex_viewer = "xdg-open"
 vim.cmd.colorscheme "catppuccin"
 vim.cmd.filetype "indent off"
+
+if vim.g.neovide then
+vim.o.guifont = "IBM Plex Mono,IBM Plex Sans TC,Noto Sans CJK TC:h28"
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_no_idle = false
+vim.g.neovide_input_ime = true
+vim.g.neovide_scale_factor = 1.0
+
+local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+    end
+    vim.keymap.set("n", "<C-=>", function()
+        change_scale_factor(1.25)
+    end)
+    vim.keymap.set("n", "<C-->", function()
+        change_scale_factor(1/1.25)
+    end)
+end
