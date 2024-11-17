13cae0cb36bfacb63ee7d73a3de0d60c94cb0fbb
CToID <funk443@yahoo.com.tw>
2024-11-17 12:02:51 +0800

    Add some keybinds for Neogit and Telescope, and minor formatting fix.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -113,20 +113,34 @@ vim.g.netrw_browsex_viewer = "xdg-open"
 vim.cmd.colorscheme "catppuccin"
 vim.cmd.filetype "indent off"
 
-if vim.g.neovide then
-vim.o.guifont = "IBM Plex Mono,IBM Plex Sans TC,Noto Sans CJK TC:h28"
-vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_no_idle = false
-vim.g.neovide_input_ime = true
-vim.g.neovide_scale_factor = 1.0
+vim.g.mapleader = " "
+vim.api.nvim_set_keymap(
+    "n", "<Leader>ff", ":Telescope fd<CR>",
+    {noremap = true, silent = true}
+)
+vim.api.nvim_set_keymap(
+    "n", "<Space>", "<NOP>",
+    {noremap = true}
+)
+vim.api.nvim_set_keymap(
+    "n", "<Leader>g", ":Neogit<CR>",
+    {noremap = true, silent = true}
+)
 
-local change_scale_factor = function(delta)
-    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+if vim.g.neovide then
+    vim.o.guifont = "IBM Plex Mono,IBM Plex Sans TC,Noto Sans CJK TC:h28"
+    vim.g.neovide_hide_mouse_when_typing = true
+    vim.g.neovide_no_idle = false
+    vim.g.neovide_input_ime = true
+    vim.g.neovide_scale_factor = 1.0
+
+    local change_scale_factor = function(delta)
+        vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+        vim.keymap.set("n", "<C-=>", function()
+            change_scale_factor(1.25)
+        end)
+        vim.keymap.set("n", "<C-->", function()
+            change_scale_factor(1/1.25)
+        end)
     end
-    vim.keymap.set("n", "<C-=>", function()
-        change_scale_factor(1.25)
-    end)
-    vim.keymap.set("n", "<C-->", function()
-        change_scale_factor(1/1.25)
-    end)
 end
