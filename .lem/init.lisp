69f550b25fabd1dd4f32cdde8f040ae0111670dc
CToID <funk443@yahoo.com.tw>
2025-01-05 03:15:26 +0800

    Enable `paredit-mode` while in SDL2.
    Don't want to enable it in terminal mode because of:
    https://github.com/lem-project/lem/issues/373
    
    Since these keybindings are crucial parts of paredit, without them
    it'll be a pain in the ass to use paredit mode.

--- .lem/init.lisp
+++ .lem/init.lisp
@@ -12,6 +12,9 @@
 
 #+lem-sdl2
 (when (typep (lem:implementation) 'lem-sdl2/sdl2:sdl2)
+  ;; Only enable `paredit-mode` when using SDL2 backend.
+  ;; See https://github.com/lem-project/lem/issues/373
+  (lem-paredit-mode:paredit-mode)
   (lem-sdl2/display:change-font (lem-sdl2/display:current-display)
                                 (lem-sdl2/font:make-font-config
                                  :size 20
