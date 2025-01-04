0e00a3958e7e8623f996a6ee84d71a2f4133276a
CToID <funk443@yahoo.com.tw>
2025-01-05 04:02:34 +0800

    Fix paredit enable issue.

--- .lem/init.lisp
+++ .lem/init.lisp
@@ -15,7 +15,7 @@
 (when (typep (lem:implementation) 'lem-sdl2/sdl2:sdl2)
   ;; Only enable `paredit-mode` when using SDL2 backend.
   ;; See https://github.com/lem-project/lem/issues/373
-  (lem-paredit-mode:paredit-mode)
+  (add-hook lem-lisp-mode:*lisp-mode-hook* #'lem-paredit-mode:paredit-mode)
   (lem-sdl2/display:change-font (lem-sdl2/display:current-display)
                                 (lem-sdl2/font:make-font-config
                                  :size 20
