563ee6ff08be0dfca88689a4333f2feb8e30a81b
CToID <funk443@yahoo.com.tw>
2025-01-05 03:07:10 +0800

    Found ways to set fonts while using SDL.

--- .lem/init.lisp
+++ .lem/init.lisp
@@ -9,6 +9,15 @@
   ("C-\\" 'redo))
 
 (lem-if:update-cursor-shape (lem:implementation) :bar)
+
+#+lem-sdl2
+(when (typep (lem:implementation) 'lem-sdl2/sdl2:sdl2)
+  (lem-sdl2/display:change-font (lem-sdl2/display:current-display)
+                                (lem-sdl2/font:make-font-config
+                                 :size 20
+                                 :latin-normal-file "/home/id/.local/share/fonts/Iosevka-Regular.ttf"
+                                 :latin-bold-file "/home/id/.local/share/fonts/Iosevka-Bold.ttf")))
+
 (lem/line-numbers:toggle-line-numbers)
 
 (remove-hook *after-init-hook* 'lem/frame-multiplexer::enable-frame-multiplexer)
