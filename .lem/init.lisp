76afd94abd8660bb5a47d89c48d962fbcb9ef57c
CToID <funk443@yahoo.com.tw>
2025-01-07 18:19:53 +0800

    Save clipboard content into killring before kill.

--- .lem/init.lisp
+++ .lem/init.lisp
@@ -5,6 +5,7 @@
 (setf lem/line-numbers:*relative-line* t
       *auto-format* t
       lem-lisp-mode/hyperspec::*hyperspec-root* "https://funk443.github.io/CLHS/HyperSpec/"
+      lem-core/commands/edit::*save-interprogram-clipboard-before-kill-p* t
       (variable-value 'add-newline-at-eof-on-writing-file :global) t
       (variable-value 'lem-core/commands/file::delete-trailing-whitespace-on-writing-file :global) t)
 
