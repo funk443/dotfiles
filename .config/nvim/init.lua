1f8e76080ae73d9c5e74d80891568358e1fb42e3
CToID <funk443@yahoo.com.tw>
2025-01-14 22:12:50 +0800

    Use a proper formatter for the file.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -57,37 +57,34 @@ lazy_plugins = {
 }
 
 lazy_opts = {
-    ui = {
-        icons = {
-            cmd = "⌘",
-            config = "🛠",
-            event = "📅",
-            ft = "📂",
-            init = "⚙",
-            keys = "🗝",
-            plugin = "🔌",
-            runtime = "💻",
-            require = "🌙",
-            source = "📄",
-            start = "🚀",
-            task = "📌",
-            lazy = "💤 ",
-        },
-    },
+	ui = {
+		icons = {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
 }
 
 require("lazy").setup(lazy_plugins, lazy_opts)
 
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	command = "%s/\\s\\+$//e",
+})
 
-vim.api.nvim_create_autocmd(
-    {"BufWritePre"},
-    {command = "%s/\\s\\+$//e"}
-)
-
-vim.api.nvim_create_autocmd(
-    {"BufWinEnter"},
-    {command = 'silent! normal! g`"zz'}
-)
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	command = 'silent! normal! g`"zz',
+})
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 0
@@ -97,41 +94,25 @@ vim.opt.showbreak = "|"
 vim.opt.undofile = true
 
 vim.g.netrw_browsex_viewer = "xdg-open"
-vim.cmd.colorscheme "catppuccin"
-vim.cmd.filetype "indent off"
+vim.cmd.colorscheme("industry")
+vim.cmd.filetype("indent off")
 
 vim.g.mapleader = " "
-vim.api.nvim_set_keymap(
-    "n", "<Leader>ff", ":Telescope fd<CR>",
-    {noremap = true, silent = true}
-)
-vim.api.nvim_set_keymap(
-    "n", "<Leader>fb", ":Telescope buffers<CR>",
-    {noremap = true, silent = true}
-)
-vim.api.nvim_set_keymap(
-    "n", "<Space>", "<NOP>",
-    {noremap = true}
-)
-vim.api.nvim_set_keymap(
-    "n", "<Leader>g", ":Neogit<CR>",
-    {noremap = true, silent = true}
-)
 
 if vim.g.neovide then
-    vim.o.guifont = "IBM Plex Mono,IBM Plex Sans TC,Noto Sans CJK TC:h30"
-    vim.g.neovide_hide_mouse_when_typing = true
-    vim.g.neovide_no_idle = false
-    vim.g.neovide_input_ime = true
-    vim.g.neovide_scale_factor = 1.0
+	vim.o.guifont = "Iosevka,Noto Sans CJK TC:h30"
+	vim.g.neovide_hide_mouse_when_typing = true
+	vim.g.neovide_no_idle = false
+	vim.g.neovide_input_ime = true
+	vim.g.neovide_scale_factor = 1.0
 
-    local change_scale_factor = function(delta)
-        vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-        vim.keymap.set("n", "<C-=>", function()
-            change_scale_factor(1.25)
-        end)
-        vim.keymap.set("n", "<C-->", function()
-            change_scale_factor(1/1.25)
-        end)
-    end
+	local change_scale_factor = function(delta)
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+		vim.keymap.set("n", "<C-=>", function()
+			change_scale_factor(1.25)
+		end)
+		vim.keymap.set("n", "<C-->", function()
+			change_scale_factor(1 / 1.25)
+		end)
+	end
 end
