2c8f7ebe35229cbcee06f18380a3c98b23f7bedd
CToID <funk443@yahoo.com.tw>
2025-02-24 07:48:35 +0800

    Do not ignore case while searching.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -54,6 +54,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(case-fold-search nil)
  '(case-replace nil)
  '(column-number-mode t)
  '(current-language-environment "UTF-8")
