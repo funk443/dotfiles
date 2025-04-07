88797742d2913debac8b8fc8fcf430d97ab1ee28
CToID <funk443@yahoo.com.tw>
2025-04-07 18:23:01 +0800

    Add notification to indicate successful screenshot.

--- .config/sway/grim.sh
+++ .config/sway/grim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 dir="$HOME/Pictures/grim"
 mkdir -p "$dir"
 
@@ -7,3 +9,5 @@ output_file="${dir}/$(date +%Y%m%d-%H%M%S).png"
 
 {[ "$1" = "--region" ] && grim -g "$(slurp)" "$output_file"} || grim "$output_file"
 wl-copy < "$output_file"
+
+notify-send -t 1000 "Screenshot saved and copied."
