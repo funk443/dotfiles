30c0540fff713e6f112ff5c051ad39b414a7cd76
CToID <funk443@yahoo.com.tw>
2025-05-12 15:25:16 +0800

    Try to live without transient-mark-mode.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -149,6 +149,7 @@
  '(sentence-end-double-space nil)
  '(shell-command-prompt-show-cwd t)
  '(tool-bar-mode nil)
+ '(transient-mark-mode nil)
  '(which-key-mode t)
  '(x-select-enable-clipboard-manager nil))
 
