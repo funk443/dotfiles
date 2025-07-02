5e9b7278314b09b18df558c9cddf96b2ea6a8a45
CToID <funk443@yahoo.com.tw>
2025-07-02 14:50:46 +0800

    Set colors from command args.

--- Windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,4 +4,9 @@ Write-Host -NoNewline "`e[?12l"
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -PredictionSource None
 
+Set-PSReadLineOption -Colors @{
+    "Default" = "White"
+    "Parameter" = "Cyan"
+}
+
 $ENV:EDITOR = "nvim"
