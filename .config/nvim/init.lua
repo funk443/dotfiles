489eb2c6ca0309a1b44c0268d6eb537507a8c07e
CToID <funk443@yahoo.com.tw>
2025-07-02 21:01:51 +0800

    Just use default theme.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,17 +37,6 @@ vim.cmd.filetype("indent off")
 -- Plugin list {{{
 
 local plugins = {
-    {
-        "catppuccin/nvim", name = "catppuccin",
-        lazy = false, priority = 1000,
-        opts = {
-            flavour = "mocha",
-        },
-        config = function(_, opts)
-            require("catppuccin").setup(opts)
-            vim.cmd.colorscheme("catppuccin")
-        end
-    },
     { "junegunn/vim-easy-align", lazy = true, cmd = "EasyAlign", opts = {} },
 }
 
