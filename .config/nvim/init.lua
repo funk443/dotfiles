7ef298a3865caf80842c7e1758f8de644d36c29e
CToID <funk443@icloud.com>
2025-11-21 02:57:04 +0800

    Set tabstop to 2 in Clojure.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -48,7 +48,7 @@ vim.filetype.add({
     },
 })
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "lisp", "scheme" },
+    pattern = { "lisp", "scheme", "clojure" },
     callback = function(ev)
         vim.opt_local.tabstop = 2
     end,
