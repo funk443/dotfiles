c5dcf6e0404a7702a3b1cdb09efdb241541c65c6
CToID <funk443@icloud.com>
2026-05-10 21:20:57 +0800

    Enable auto line breaks in text files.
    https://github.com/funk443/DDoc

--- .config/nvim/lua/AutoCmds.lua
+++ .config/nvim/lua/AutoCmds.lua
@@ -2,3 +2,11 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     command = "%s/\\s\\+$//e",
     desc = "Remove trailing whitespaces before saving.",
 })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "text" },
+    callback = function(ev)
+        vim.opt_local.formatoptions:append("t")
+    end,
+    desc = "Enable auto line breaks for text files."
+})
