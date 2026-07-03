a5b0598883c167574436d747b9c72508dc7fc126
CToID <funk443@icloud.com>
2026-07-03 08:55:35 +0800

    Fix for WSL.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -91,4 +91,26 @@ up after the user logs out.")
 
 (id-set-fonts)
 
+;; WSLg breaks copy-paste from Emacs into Windows
+;; see: https://www.lukas-barth.net/blog/emacs-wsl-copy-clipboard/
+
+(defvar wl-copy-process nil)
+
+(defun wsl-copy (text)
+  (unless (and wl-copy-process (process-live-p wl-copy-process))
+    (setq wl-copy-process (make-process :name "wl-copy"
+                                        :buffer nil
+                                        :command '("wl-copy" "-f" "-n")
+                                        :connection-type 'pipe)))
+  (process-send-string wl-copy-process text)
+  (process-send-eof wl-copy-process))
+
+(defun wsl-paste ()
+  (if (and wl-copy-process (process-live-p wl-copy-process))
+      nil
+    (shell-command-to-string "wl-paste -n | tr -d \r")))
+
+(setq interprogram-cut-function #'wsl-copy
+      interprogram-paste-function #'wsl-paste)
+
 (put 'narrow-to-region 'disabled nil)
