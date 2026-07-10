2498cacc2974d259f150565c4dd4adb8251be4c2
CToID <funk443@icloud.com>
2026-07-10 20:50:37 +0800

    Display human-readable size in the buffer menu.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -70,6 +70,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Buffer-menu-human-readable-sizes t)
  '(auth-source-save-behavior nil)
  '(column-number-mode t)
  '(current-language-environment "UTF-8")
