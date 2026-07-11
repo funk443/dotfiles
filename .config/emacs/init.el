75247435b932b55d8c1c8cf60bbbeda3da3183d0
CToID <funk443@icloud.com>
2026-07-11 18:26:09 +0800

    Revert "Remove unneeded face customizations."
    This reverts commit 58b25b4f3cc1bbd3746e956d63b51a879fb4aee0.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -106,6 +106,6 @@ up after the user logs out.")
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :slant normal :weight regular :height 203 :family "Roboto Mono"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#000000" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 203 :width normal :foundry "GOOG" :family "Roboto Mono"))))
  '(fixed-pitch ((t (:inherit default :family "Roboto Mono"))))
  '(variable-pitch ((t (:inherit default :family "Roboto")))))
