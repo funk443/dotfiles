81181c14de4aba01b283dbc77caa9f484dc09b84
CToID <funk443@icloud.com>
2026-07-11 15:50:27 +0800

    Shouldn't need to set fixed-pitch-serif face.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -7,9 +7,6 @@
   (set-face-attribute 'fixed-pitch nil
                       :family "Roboto Mono"
                       :inherit 'default)
-  (set-face-attribute 'fixed-pitch-serif nil
-                      :family "Courier Prime Code"
-                      :inherit 'default)
   (set-face-attribute 'variable-pitch nil
                       :family "Roboto"
                       :inherit 'default)
