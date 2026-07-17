c77e2dc6c82ce3e5f9c26b723482f7e2567b3e04
CToID <funk443@icloud.com>
2026-07-17 20:14:11 +0800

    Enable narrow-to-region.
    It's quite useful.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -127,3 +127,5 @@ up after the user logs out.")
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(put 'narrow-to-region 'disabled nil)
