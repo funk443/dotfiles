d6f3f10f13eab60336020e8ce2c94bc7092f6473
CToID <funk443@icloud.com>
2026-08-18 08:47:32 +0800

    Move custom settings to a separate file.

---
+++ .config/emacs/custom.el
@@ -0,0 +1,47 @@
+;; -*- lexical-binding: t; -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(Buffer-menu-human-readable-sizes t)
+ '(auth-source-save-behavior nil)
+ '(column-number-mode t)
+ '(current-language-environment "UTF-8")
+ '(custom-enabled-themes '(modus-vivendi))
+ '(delete-selection-mode t)
+ '(dired-dwim-target 'dired-dwim-target-next)
+ '(dired-listing-switches "-alh")
+ '(eshell-visual-commands
+   '("vi" "vim" "nvim" "screen" "tmux" "top" "htop" "less" "more" "lynx" "links"
+     "ncftp" "ncmpcpp" "mutt" "pine" "tin" "trn" "elm" "mpv"))
+ '(fill-column 80)
+ '(frame-resize-pixelwise t)
+ '(grep-command "rg -nH --no-heading --null -e ")
+ '(grep-use-null-device nil)
+ '(indent-tabs-mode nil)
+ '(indicate-empty-lines t)
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(minibuffer-visible-completions t)
+ '(package-selected-packages '(fd-dired magit paredit))
+ '(require-final-newline t)
+ '(save-interprogram-paste-before-kill t)
+ '(savehist-additional-variables '(register-alist))
+ '(savehist-mode t)
+ '(scroll-bar-mode nil)
+ '(sentence-end-double-space nil)
+ '(shell-command-prompt-show-cwd t)
+ '(tool-bar-mode nil)
+ '(tramp-use-connection-share nil)
+ '(treesit-enabled-modes t)
+ '(which-key-mode t)
+ '(x-select-enable-clipboard-manager nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
